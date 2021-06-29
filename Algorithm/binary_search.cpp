6a42f1ae041bad42cf8c262d972d90e6f0b4e6fc
kaizerstudio <truongsondotcom@outlook.com>
2021-06-29 15:23:34 +0700

    fix #3

--- Algorithm/binary_search.cpp
+++ Algorithm/binary_search.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int binsrc(int *arr, const int &num){
+    int l = 0, r = sizeof(arr)/ sizeof(arr[0]) - 1;
+    int mid = l + (l - r) / 2;
+    if (num > arr[r] || num < arr[l]) return -1;
+    while (arr[mid] != num)
+    {
+        if (arr[mid] < num) l = mid + 1;
+        else r = mid - 1;
+        mid = l + (l - r) / 2;
+    }
+    return mid;
+}
+
 int main(){
-    int a;
-    cin >> a;
-    
+    int a[5] = { 2, 3, 4, 10, 40 };
+    int num = 10;
+    cout << binsrc(a, num);
+    return 0;
 }
--- Stage 5/4.cpp
+++ Stage 5/4.cpp
@@ -3,65 +3,18 @@ using namespace std;
 
 #define FIN(dir) freopen(dir, "r", stdin)
 
-void lps_add(const string &pat, int N, int *lps);
-
-int kmp_search(const string &txt, const string &pat){
-    int count = 0;
-    int M = txt.length();
-    int N = pat.length();
-    int lps[N];
-    lps_add(pat, N, lps);
-    int i = 0, j = 0;
-    while (i < M){ 
-        if (txt[i] == pat[j]){
-            ++i;
-            ++j;
-        }
-        if (j == N){
-            ++count;
-            j = lps[j - 1];
-        }
-        else{
-            if (pat[j] != txt[i]){
-                if (j != 0) j = lps[j - 1];
-                else ++i;
-            }
-        } 
-    }
-    return count;
-}
-
-void lps_add(const string &pat, int N, int *lps){
-    int len = 0;
-    int i = 1; 
-    lps[0] = 0;
-    while (i < N)
-    {
-        if (pat[i] == pat[len]){
-            ++len;
-            lps[i] = len;
-            ++i;
-        }
-        else{
-            if (len != 0){
-                len = lps[len - 1];
-            }
-            else { 
-                lps[i] = len;
-                ++i;
-            }
-        }
-    }    
-}
-
 int main() {
     FIN("Leutrai.txt");
     string in;
     int count = 0;
     while (cin >> in)
     {
+        int i = 0;
         in = '.' + in + '.';
-        cout << kmp_search(in, ".1.") - 1 << endl;
+        for (int i = 0; i < in.length() - 2; ++i)
+        {
+            if (in.substr(i, 3) == ".1.") ++count;
+        }
     }
     cout << count;
     return 0;
