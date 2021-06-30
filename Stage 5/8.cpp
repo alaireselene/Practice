bf3b4235562ebafab763fbf3c8ed1e5e2573a774
kaizerstudio <truongsondotcom@outlook.com>
2021-06-30 16:54:56 +0700

    :))

--- Stage 5/8.cpp
+++ Stage 5/8.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void multiple(string &a, string b){
+    vector <int> store;
+    int alen = a.length(), blen = b.length();
+    for (int k = blen - 1; k >= 0; --k){
+        for (int i = alen - 1; i >= 0; --i){
+            int temp = ((int) b[k]) * ((int) a[k]); 
+        }
+    }
+}
 int main(){
     // Code goes here;
 }
