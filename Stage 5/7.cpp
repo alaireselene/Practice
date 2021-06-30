b6d59d6130dc4526ed3db01c12567b7ca5f7e837
kaizerstudio <truongsondotcom@outlook.com>
2021-06-30 16:15:18 +0700

    :))

--- Stage 5/7.cpp
+++ Stage 5/7.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector <int> convert(string &a){
-    int count = 0;
-    vector <int> ans;
-    for (int i = a.length() - 1; i >= 0; --i){
-        for (int k = 0; i < a.length() - 1; ++i){
+void convert(const string &a, vector <int> &v){
+    int len = a.length();
+    for (int i = len - 1; i >= 0; --i){
+        int count = 0;
+        for (int k = 0; k < i; ++k){
             if (a[k] < a[i]) ++count;
         }
+        v.push_back(count);
     }
 }
 
 int main(){
     string num = "264153";
-    int n = num.length(), count = 0;
-    int lps[n];
-    lps[0] = 0;
-    for (int i = 1; i < n; ++i){
-        if (num[i] > num[i + 1]){
-            lps[i] = count;
-        }
+    vector<int> ans;
+    convert(num, ans);
+    for (int i = 0; i < sizeof(ans) / sizeof(ans[1]); ++i){
+        cout << ans[i] << '\t';
     }
+    cout << '\n';
+    return 0;
 }
