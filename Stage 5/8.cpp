1e252dc9788d5c056ae8634a58dcd960721587d5
kaizerstudio <truongsondotcom@outlook.com>
2021-06-30 16:55:42 +0700

    :))

--- Stage 5/8.cpp
+++ Stage 5/8.cpp
@@ -6,7 +6,9 @@ void multiple(string &a, string b){
     int alen = a.length(), blen = b.length();
     for (int k = blen - 1; k >= 0; --k){
         for (int i = alen - 1; i >= 0; --i){
-            int temp = ((int) b[k]) * ((int) a[k]); 
+            int temp = ((int) b[k]) * ((int) a[k]);
+            int add = temp /10;
+            temp %= 10; 
         }
     }
 }
