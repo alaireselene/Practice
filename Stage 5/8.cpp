280fd1c869769c642f8f7749d6b1d6c873b69b07
kaizerstudio <truongsondotcom@outlook.com>
2021-06-30 22:45:53 +0700

    :))

--- Stage 5/8.cpp
+++ Stage 5/8.cpp
@@ -1,17 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void multiple(string &a, string b){
-    vector <int> store;
-    int alen = a.length(), blen = b.length();
-    for (int k = blen - 1; k >= 0; --k){
-        for (int i = alen - 1; i >= 0; --i){
-            int temp = ((int) b[k]) * ((int) a[k]);
-            int add = temp /10;
-            temp %= 10; 
-        }
+void get_right (string &a, string &b){
+    if (a.size() >= b.size()){
+        b.insert(0, a.size() - b.size(), '0');
+    }
+    else{
+        a.insert(0, b.size() - a.size(), '0');
     }
 }
+
+string big_add(string a, string b){
+    string ans;
+    get_right(a, b);
+    int temp = 0;
+    for (int i = a.size() - 1; i >= 0; --i){
+        int num = a[i] + b[i] - '0' - '0' + temp;
+        char ch = int(num % 10) + '0';
+        ans.insert(0, 1, ch);
+        temp = num / 10;
+        if (i == 0 && temp > 0) ans.insert(0, 1, char(temp + '0'));
+    }
+    return ans;
+}
+string smol_mul(string a, char b){
+    string ans;
+    int temp = 0;
+    for (int i = a.size() - 1; i >= 0; --i){
+        int num = (b - '0') * (a[i] - '0') + temp;
+        char ch = int(num % 10) + '0';
+        ans.insert(0, 1, ch);
+        temp = num / 10;
+        if (i == 0 && temp > 0) ans.insert(0, 1, char(temp + '0'));
+    }
+    return ans;
+}
+string big_mul(string a, string b){
+    string ans = "";
+    string preans[a.length()];
+    for (int i = a.length() - 1; i <= 0; --i){
+        preans[i] = smol_mul(b, a[i]);
+        preans[i].insert(preans[i].end(), a.length() - i - 1, '0');
+        ans = big_add(ans, preans[i]); 
+    }
+    return ans;
+}
+string big_mul(string a, string b);
 int main(){
-    // Code goes here;
+    string a = "99";
+    string b = "99";
+    char c = '9';
+    cout << big_add(a, b) << '\t';
+    cout << smol_mul(a, c) << '\t';
+    cout << big_mul(a, b) << '\n';
+    return 0;
 }
