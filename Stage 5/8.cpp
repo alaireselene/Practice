ba512b8ddb183819f340772c8a2ab979938337c4
kaizerstudio <truongsondotcom@outlook.com>
2021-07-01 11:34:55 +0700

    fixed #5

--- Stage 5/8.cpp
+++ Stage 5/8.cpp
@@ -1,21 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void get_right (string &a, string &b){
-    // Hàm có chức năng san đều độ dài 2 xâu a, b bằng cách thêm
-    // bớt 0 vào xâu có độ dài nhỏ hơn.
-    if (a.size() >= b.size()){
-        b.insert(0, a.size() - b.size(), '0');
-    }
-    else{
-        a.insert(0, b.size() - a.size(), '0');
-    }
-}
-
 string big_add(string a, string b){
     // Hàm cộng 2 số lớn.
     string ans;
-    get_right(a, b);
+    a.insert(0, b.size() - a.size(), '0');
     // San đều
     int temp = 0;
     // 5 + 9 = 14, temp sẽ bằng 1;
@@ -60,19 +49,22 @@ string big_mul(string a, string b){
     // với xâu b bằng hàm smol_mul.
     // sau đó, chèn thêm chữ số 0 vào
     // sau và cộng với string ans
-    string ans = "";
+    string ans;
     string preans[a.length()];
-    for (int i = a.length() - 1; i <= 0; --i){
+    for (int i = a.length() - 1; i >= 0; --i){
         preans[i] = smol_mul(b, a[i]);
         preans[i].insert(preans[i].end(), a.length() - i - 1, '0');
         ans = big_add(ans, preans[i]); 
     }
     return ans;
 }
+string giaithua(string a){ 
+    string b = to_string(stoi(a) - 1);
+    if (b == "0") return "1";
+    else return big_mul(a, giaithua(b));
+}
 int main(){
-    string a = "16239847612389347632189462189734762189463278476";
-    string b = "99999999999999999999999999111111192929399999999";
-    char c = '9';
-    cout << big_add(a, b) << '\t';
+    string a = "5";
+    cout << giaithua(a) << '\t';
     return 0;
 }
