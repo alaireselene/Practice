bc3da5917167db6ccb1768e355395f89707c9163
kaizerstudio <truongsondotcom@outlook.com>
2021-07-01 10:52:54 +0700

    :))

--- Stage 5/8.cpp
+++ Stage 5/8.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 void get_right (string &a, string &b){
+    // Hàm có chức năng san đều độ dài 2 xâu a, b bằng cách thêm
+    // bớt 0 vào xâu có độ dài nhỏ hơn.
     if (a.size() >= b.size()){
         b.insert(0, a.size() - b.size(), '0');
     }
@@ -11,15 +13,30 @@ void get_right (string &a, string &b){
 }
 
 string big_add(string a, string b){
+    // Hàm cộng 2 số lớn.
     string ans;
     get_right(a, b);
+    // San đều
     int temp = 0;
+    // 5 + 9 = 14, temp sẽ bằng 1;
+    // temp = 0 vì 2 chữ số trong cùng
+    // cộng với nhau không cần nhớ.
     for (int i = a.size() - 1; i >= 0; --i){
         int num = a[i] + b[i] - '0' - '0' + temp;
+        // Giá trị đại số của char n là n - '0'.
+        // Áp dụng nguyên lý này, lấy tổng giá trị 2 phần
+        // tử cùng vị trí của xâu.
         char ch = int(num % 10) + '0';
+        // Giá trị kiểu char của số n là n + '0'.
+        // Chỉ lấy phần hàng đơn vị.
         ans.insert(0, 1, ch);
+        // Chèn vào xâu
         temp = num / 10;
+        // Lấy phần nhớ
         if (i == 0 && temp > 0) ans.insert(0, 1, char(temp + '0'));
+        // Nếu cộng đến hàng ngoài cùng phát sinh số lớn hơn 10, ví dụ:
+        // 943 + 120 -> 1163. Phần if này để sau khi lấy phần đơn vị
+        // thì chèn thêm 1 vào.
     }
     return ans;
 }
