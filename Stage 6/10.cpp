cd87cc8520b289bcb863e835420f3251ec3d0163
kaizerstudio <truongsondotcom@outlook.com>
2021-07-07 17:11:34 +0700

    10

--- Stage 6/10.cpp
+++ Stage 6/10.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
-    int n = 12, pos, len = 0, comp = 0;
+    int n, pos, len = 0, comp = 0;
     // Do dai mac dinh la 0.
     // Bien comp dung de so sanh do dai
-    float a[n] = {53, 94, 73, 66, 83, 12, 42, 50, 2, 78, 18, 64};
+    cin >> n;
+    float a[n];
+    for (float &item:a) cin >> item;
     for (int i = 0; i < n - 1; ++i){
         if (a[i] < a[i + 1]) ++len;
         else len = 0;
@@ -17,6 +19,6 @@ int main(){
     }
     cout << pos << "\t" << comp << "\n";
     if (comp == 0) cout << "INVALID \n";
-    else cout << "Day Max tu " << pos + 1 - comp << " den " << pos << "\n";
+    else cout << "Day Max tu " << pos + 1 - comp << " den " << pos + 1 << "\n";
     return 0;
 }
