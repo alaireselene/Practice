bd959d8af0e4b244aad5e46a622bbfeb44fb64f8
kaizerstudio <truongsondotcom@outlook.com>
2021-07-17 10:25:10 +0700

    Update

---
+++ Stage 6/18.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> s;
+
+bool isLarger(const string &a, const string &b){
+    return a + b > b + a;
+}
+
+void inp(){
+    fstream FIN;
+    FIN.open("NUMCON.INP", ios::in);
+    
+    while (!FIN.eof()){
+        string n;
+        FIN >> n;
+        s.push_back(n);
+    }
+    FIN.close();
+}
+
+int main(){
+    fstream FOUT;
+    FOUT.open("NUMCON.OUT", ios::out);
+    inp();
+    sort(s.begin(), s.end(), isLarger);
+    for (auto &item:s) FOUT << item;
+    cout << '\n';
+    FOUT.close();
+    return 0;
+}
