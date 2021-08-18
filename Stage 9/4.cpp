076e511ff459f752113bcc70ac96e03400ade582
kaizerstudio <truongsondotcom@gmail.com>
2021-08-19 05:07:08 +0700

    Breh

--- Stage 9/4.cpp
+++ Stage 9/4.cpp
@@ -11,8 +11,21 @@ int main(){
         cin >> x;
         ++in[x];
     }
-    sum += (min(in[1], in[3]) + abs(in[1] - in[3]));
-    sum += (in[2] / 2 + (in[2] % 2 > 0));
-    sum += in.count(4);
-    cout << sum;
+    sum += (in[1] / 4 + in[2] / 2 + in[4]);
+    in[1] -= in[1] / 4;
+    in[2] -= int(in[2] / 2.0) * 2;
+
+    int a = min(in[1], in[3]);
+    sum += a;
+    in[1] -= a;
+    in[3] -= a;
+    if (in[3] > in[1]) sum += in[3];
+
+    int b = min(in[1] / 2, in[2]);
+    sum += b;
+    in[1] -= 2 * b;
+    in[2] -= b;
+    
+    if (in[1] != 0 || in[2] != 0) sum += 1;
+    cout << sum << '\n';
 }
