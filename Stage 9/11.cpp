5ca0927d5f5bdef91a6d1d54ab64c52025894fc6
kaizerstudio <truongsondotcom@outlook.com>
2021-08-28 18:56:43 +0700

    lmao

--- Stage 9/11.cpp
+++ Stage 9/11.cpp
@@ -4,37 +4,28 @@ typedef uint64_t u64;
 
 #define TASK "SUB"
 
-vector<u64> in, sum;
-u64 n, s, c = INT64_MAX;
+vector<u64> sum;
+u64 n, s, x, c = INT64_MAX;
 
-void work(const u64 &k){
-    u64 l = k, r = n;
-    while (l <= r){
-        u64 mid = (l + r) / 2;
-        if (sum[mid] - sum[k - 1] >= s)
-            r = mid - 1;
-        else l = mid + 1;
-    }
-    c = min(c, l - k + 1);
-}
-
-int main(){
-    freopen(TASK".INP", "r", stdin);
-    freopen(TASK".OUT", "w", stdout);
+int main() {
+    //freopen(TASK".INP", "r", stdin);
+    //freopen(TASK".OUT", "w", stdout);
 
     cin >> n >> s;
-    for (u64 i = 0, x; i < n; ++i){
-        cin >> x;
-        in.push_back(x);
-    }
-
     sum.push_back(0);
-    for (u64 i = 0; i < n; ++i){
-        sum.push_back(in[i] + sum[i]);
+    for (u64 i = 1; i <= n; ++i) {
+    	cin >> x;
+        sum.push_back(sum[i - 1] + x);
     }
-
-    for (u64 i = 1; i <= n; ++i)
-        work(i);
+    
+    for (u64 l = 0; l < n; ++l) {
+    	auto iter_r = lower_bound(sum.begin(), sum.end(), s + sum[l]);
+    	if (iter_r == sum.end()) continue;
+    	else {
+    		u64 r = iter_r - sum.begin();
+    		c = min(c, r - l);
+    	}
+    }
 
     cout << c;
     return 0;
