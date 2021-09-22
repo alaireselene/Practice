9ef7ea93c5f818db7d2df43000ef73bc8f983cf1
Sena Alaire <truongsondotcom@gmail.com>
2021-09-22 22:04:52 +0700

    Arghhhhhh

--- Basic 1/4.cpp
+++ Basic 1/4.cpp
@@ -6,13 +6,21 @@ struct pos {
     int y;
 };
 
+int double_area(const pos &a, const pos &b, const pos &c) {
+    return abs((b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y));
+}
+
 void test() {
     vector<pos> t(3);
     for (auto &item:t) {
         cin >> item.x >> item.y;
     }
-
-    //
+    int inbound = 0;
+    int _s = double_area(t[0], t[1], t[2]);
+    for (int i = 1; i < 3; ++i) {
+        inbound += __gcd(abs(t[i].x - t[i - 1].x), abs(t[i].y - t[i - 1].y)) + __gcd(abs(t[0].x - t[2].x), abs(t[0].y - t[2].y));
+    }
+    cout << ceil((_s + 2 - inbound) / 2.0) << '\n';
 }
 
 int main() {
