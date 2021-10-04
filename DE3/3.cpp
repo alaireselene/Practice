a0214d5830aba2d4d298c542eb23a58158aed8e5
Sena Alaire <truongsondotcom@gmail.com>
2021-10-04 22:01:31 +0700

    No more stupid

--- DE3/3.cpp
+++ DE3/3.cpp
@@ -1,23 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-uint64_t a, b, g, foo;
+int a, b, g;
+map<int, int> m;
 string s;
-map <uint64_t, uint64_t> check;
+
+void correct (int &a, int &b) {
+	g = __gcd(a, b);
+	a /= g;
+	b /= g;
+}
 
 int main() {
     cin >> a >> b;
-    g = __gcd(a, b);
-    a /= g;
-    b /= g;
-    foo = a;
-    if (b % 5 == 0 || b % 2 == 0) cout << double(a) / b;
-    else while (!check[foo]) {
-        foo = a % b;
-        check[foo] = 1;
-        s.push_back('0' + a / b);
-        a = foo = foo * 10;
-
+    correct(a, b);
+    if (b % 5 == 0 || b % 2 == 0) {
+		cout << double(a) / b << endl;
+	}
+    else {
+        while(true) {
+			int temp = a % b;
+			s.push_back('0' + (a / b));
+			if (m[temp]) break;
+			else { 
+				m[temp] = 1;
+				a = (a % b) * 10;
+			}
+        }
+        cout << s[0] << ".(" << s.substr(1, s.size() - 1) << ')'<< endl;
     }
-    cout << s << endl;
+    return 0;
 }
