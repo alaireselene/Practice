50828a20ddced66583d190ceb296a8306b100d73
Sena Alaire <truongsondotcom@gmail.com>
2021-10-05 11:14:18 +0700

    Oppsie that shit is illegal

--- DE3/3.cpp
+++ DE3/3.cpp
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b, g;
-map<int, int> m;
-string s;
+int sum = 0, MAX;
+const int digit[4] = {1, 3, 7, 9};
 
-void correct (int &a, int &b) {
-	g = __gcd(a, b);
-	a /= g;
-	b /= g;
+bool is_prime(const uint64_t &n) {
+	for (uint64_t i = 2; i * i <= n; ++i) {
+		if (n % i == 0) return 0;
+	}
+	return 1;
+}
+
+void prime_generate(const uint64_t &num) {
+	uint64_t top = int(log10(num) + 1);
+	if (top == MAX) ++sum;
+	else for (int i = 1; i <= 9; ++i) {
+		uint64_t check = i * pow(10, top) + num;
+		if (is_prime(check)) prime_generate(check);
+	}
 }
 
 int main() {
-    cin >> a >> b;
-    correct(a, b);
-    if (b % 5 == 0 || b % 2 == 0) {
-		cout << double(a) / b << endl;
-	}
-    else {
-        while(true) {
-			int temp = a % b;
-			s.push_back('0' + (a / b));
-			if (m[temp]) break;
-			else { 
-				m[temp] = 1;
-				a = (a % b) * 10;
-			}
-        }
-        cout << s[0] << ".(" << s.substr(1, s.size() - 1) << ')'<< endl;
-    }
-    return 0;
+	cin >> MAX;
+	if (MAX == 1) sum = 4;
+	else for (const int &item:digit) prime_generate(item);
+	cout << sum;
+	return 0;
 }
