224d26078b282d40e6f629d9ae8af099d781508d
Sena Alaire <truongsondotcom@gmail.com>
2021-10-18 10:00:21 +0700

    lmao

--- Test 6/BAI4.cpp
+++ Test 6/BAI4.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-	freopen("BAI4.INP", "r", stdin);
-	freopen("BAI4.OUT", "w", stdout);
-	int n, div, max_len = INT_MIN;
+	// freopen("BAI4.INP", "r", stdin);
+	// freopen("BAI4.OUT", "w", stdout);
+	uint64_t n, div, max_len = INT_MIN;
 	cin >> n >> div;
-	vector<int> sum;
+	vector<uint64_t> sum;
 	sum.push_back(0);
-	for (int i = 0, x; i < n; ++i) {
+	for (uint64_t i = 0, x; i < n; ++i) {
 		cin >> x;
 		sum.push_back(x + sum.back());
 	}
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 0; j <= i; ++j) {
+	for (uint64_t i = 1; i <= n; ++i) {
+		for (uint64_t j = 0; j <= i; ++j) {
 			if ((sum[i] - sum[j]) % div == 0) max_len = max(max_len, i - j);
 		}
 	}
